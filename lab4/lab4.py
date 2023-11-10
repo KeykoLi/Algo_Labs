0632c8ac27772e4a9837d1eb22c9bc4ac58915c6
KeykoLi <119291961+KeykoLi@users.noreply.github.com>
2023-11-10 13:40:05 +0200

    add grafh and checks method

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,43 +1,122 @@
-from collections import deque
+from queue import Queue
 
-# Функція для зчитування матриці з файлу
-def read_input_matrix(filename):
-    matrix = []
-    with open(filename, 'r') as file:
-        for line in file:
-            row = [int(cell) for cell in line.strip()]
-            matrix.append(row)
-    return matrix
+def is_valid(x, y, columns, rows):
+    # Перевіряє, чи координати (x, y) знаходяться в межах матриці MxN
+    return 0 <= x < columns and 0 <= y < rows
 
+def converting_one_to_zero(matrix, update_condition):
+    columns, rows = len(matrix), len(matrix[0])
+    # Використовуємо вказану умову для заміни значень у копії матриці
+    matrix_copy = [[update_condition(matrix, x, y, columns, rows) for y in range(rows)] for x in range(columns)]
+    return matrix_copy
 
-# Функція для знаходження найкоротшого шляху за допомогою BFS
-def find_shortest_path(matrix):
-    rows, cols = len(matrix), len(matrix[0])
-    # Ініціалізуємо чергу для BFS та матрицю відстаней
-    queue = deque([(i, 0) for i in range(rows) if matrix[i][0] == 1])
-    distance = [[-1] * cols for _ in range(rows)]
+def check_and_change_zero_point(matrix, x, y, columns, rows):
+    # Перевіряє, чи є сусід зі значенням 0, якщо так - повертає 0, в іншому випадку - поточне значення матриці
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+            new_x, new_y = x + dx, y + dy
+            if is_valid(new_x, new_y, columns, rows) and matrix[new_x][new_y] == 0:
+                return 0
+    return matrix[x][y]
 
-    while queue:
-        x, y = queue.popleft()
-        # Якщо поточна клітинка знаходиться в останньому стовпці, то шлях знайдено
-        if y == cols - 1:
-            return distance[x][y] + 1
-        # Перевірка всіх можливих напрямків руху
-        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            nx, ny = x + dx, y + dy
-            # Перевірка чи нова клітинка знаходиться в межах матриці та є доступною
-            if 0 <= nx < rows and 0 <= ny < cols and matrix[nx][ny] == 1 and distance[nx][ny] == -1:
-                distance[nx][ny] = distance[x][y] + 1
-                queue.append((nx, ny))
-    # Якщо шлях не знайдено, повернути -1
-    return -1
+def graph_from_matrix(matrix):
+    columns, rows = len(matrix), len(matrix[0])
+    graph = {}
 
-# Функція для запису результату у файл
-def write_output(filename, result):
-    with open(filename, 'w') as file:
-        file.write(str(result))
+    for i in range(columns):
+        for j in range(rows):
+            if matrix[i][j] == 1:
+                # Створює список сусідів для кожної одиниці у матриці
+                neighbors = [(ni, nj) for ni, nj in [(i + dx, j + dy) for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]] if
+                             is_valid(ni, nj, columns, rows) and matrix[ni][nj] == 1]
+                graph[(i, j)] = neighbors
+    return graph
+
+def is_not_fully_zero(matrix):
+    rows = len(matrix)
+    first_column = [matrix[i][0] for i in range(rows)]
+    last_column = [matrix[i][-1] for i in range(rows)]
+
+    # Перевірка першого стовпця
+    if all(value == 0 for value in first_column):
+        if all (value == 0 for value in last_column):
+            return False
+        else:
+            return True
+    else:
+        return True
+
+def bfs(graph, start, target_column):
+    queue = Queue()
+    visited = set()
+    parent_map = {}
+
+    #починаємо з стартової вершини, додаємо її до черги та відзначаємо як відвідану
+    queue.put((start, 0))
+    visited.add(start)
+
+    while not queue.empty():
+        current_node, distance = queue.get()
+
+        if current_node[1] == target_column:
+            # Відтворюємо шлях від кінцевої вершини до початкової
+            path = []
+            while current_node:
+                path.insert(0, current_node)
+                current_node = parent_map.get(current_node)
+            return distance, path
+
+        for neighbor in graph[current_node]:
+            if neighbor not in visited:
+                queue.put((neighbor, distance + 1))
+                visited.add(neighbor)
+                parent_map[neighbor] = current_node
+
+    return -1, []
+
+def shortest_path_graph(graph, m, matrix):
+    check_zero = is_not_fully_zero(matrix)
+    if check_zero == True:
+        return -1
+    counter = 0
+    def bfs_w(node):
+        # Збільшуємо лічильник при кожному виклику BFS
+        nonlocal counter
+        counter += 1
+        return bfs(graph, node, m - 1)
+
+    min_path_len = float('inf')
+    min_path = []
+
+    for node in graph:
+        if node[1] == 0:
+            # Знаходимо найкоротший шлях для кожної вершини, що знаходиться у першому стовпці
+            path_len, path = bfs_w(node)
+            if path_len != -1:
+                # Оновлюємо мінімальну довжину та маршрут, якщо поточний шлях коротший
+                if path_len < min_path_len:
+                    min_path_len = path_len
+                    min_path = path
+
+        if min_path == m - 1:
+            # Якщо знайдено шлях до кінцевої вершини, завершуємо виконання
+            return min_path_len, counter
+
+    return (min_path_len, counter, min_path) if min_path_len != float('inf') else (-1, counter)
 
 if __name__ == "__main__":
-    input_matrix = read_input_matrix("input.txt")
-    shortest_path_length = find_shortest_path(input_matrix)
-    write_output("output.txt", shortest_path_length)
+    with open("input.txt", "r") as f:
+        matrix = []
+        for line in f.readlines():
+            # Зчитуємо вхідний файл і формуємо матрицю
+            row = [int(i) for i in line.split()]
+            matrix.append(row)
+    matrix = converting_one_to_zero(matrix, check_and_change_zero_point)
+    graph = graph_from_matrix(matrix)
+    res = shortest_path_graph(graph, len(matrix[0]), matrix)
+
+    with open("output.txt", "w") as f:
+        # Записуємо результат у вихідний файл
+        f.write(str(res))
--- lab4/test.py
+++ lab4/test.py
@@ -1,33 +1,22 @@
 import unittest
 
-from lab4 import find_shortest_path, read_input_matrix
+from lab4 import converting_one_to_zero, check_and_change_zero_point,graph_from_matrix, shortest_path_graph
 
 
 class MyTestCase(unittest.TestCase):
     def test_funct(self):
         test_matrix = [
-            [0, 0, 1, 0, 0, 0, 0, 1, 1, 1],
-            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-            [1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
-            [0, 0, 0, 1, 0, 0, 0, 1, 1, 1],
-            [0, 0, 0, 1, 1, 1, 1, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-            [1, 1, 1, 1, 0, 0, 0, 1, 0, 0],
-            [1, 1, 1, 1, 0, 0, 0, 1, 1, 1]
+            [0, 1, 1, 1, 0, 1],
+            [1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 1, 1, 1],
+            [0, 1, 1, 1, 1, 1]
         ]
+        matrix_t = converting_one_to_zero(test_matrix, check_and_change_zero_point)
+        graph = graph_from_matrix(matrix_t)
+        res = shortest_path_graph(graph, len(test_matrix[0]))
 
-        # Збережемо тестову матрицю в файл "test_input.txt"
-        with open("test_input.txt", "w") as file:
-            for row in test_matrix:
-                file.write("".join(map(str, row)) + "\n")
-
-        # Запустимо алгоритм на тестових даних
-        input_matrix = read_input_matrix("test_input.txt")
-        shortest_path_length = find_shortest_path(input_matrix)
-        self.assertEqual(shortest_path_length, 12)
+        self.assertEqual((7,1), res)
 
 
 
